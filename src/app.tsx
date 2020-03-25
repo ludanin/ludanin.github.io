45247718fadfcf76d1cc7c953fc2408d5c7e951d
Lukas Danin <lukas.danin@gmail.com>
2020-03-25 15:24:39 -0300

    Sketches app.tsx

---
+++ src/app.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "css/animations.css";
+import "css/colors.css";
+import "css/fonts.css";
+import "css/root.css";
+import "css/scrollbars.css";
+import "css/shadows.css";
+
+const App: React.FC = () => {
+  return (
+    <div/>
+  );
+};
+
+export default App;
