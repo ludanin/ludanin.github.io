07f400da01fca3dcee71224d534ea5bc90257d3e
Lukas Danin <lukas.danin@gmail.com>
2020-03-26 18:47:59 -0300

    Updates for latest changes

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,11 @@
 import React from "react";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import createSagaMiddleware from "redux-saga";
+
+import reducers from "reducers";
+import Navigator from "routes/navigator";
+import sagas from "sagas";
 
 import "css/animations.css";
 import "css/colors.css";
@@ -7,9 +14,15 @@ import "css/root.css";
 import "css/scrollbars.css";
 import "css/shadows.css";
 
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(reducers, applyMiddleware(sagaMiddleware));
+sagaMiddleware.run(sagas);
+
 const App: React.FC = () => {
   return (
-    <div/>
+    <Provider store={store}>
+      <Navigator/>
+    </Provider>
   );
 };
 
