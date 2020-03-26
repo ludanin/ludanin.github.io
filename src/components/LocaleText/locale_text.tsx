51c87c44de955fa5e30c5cede17096d80bbad904
Lukas Danin <lukas.danin@gmail.com>
2020-03-26 18:46:11 -0300

    Implements LocaleText

---
+++ src/components/LocaleText/locale_text.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+import { RPropsRedux } from "reducers";
+
+import styles from "./styles.module.css";
+
+interface Props {
+  className?: string;
+  animate?: boolean;
+  pt: React.ReactNode;
+  en: React.ReactNode;
+}
+
+/**
+ * Use this component to automatically create translateable texts.
+ *
+ * By default it animates the translation process, to disable this behavior
+ * simply pass the prop `animate` as `false`.
+ *
+ * You can pass custom a `className` to this component, with the prop
+ * `className`.
+ */
+const LocaleText: React.FC<Props> = (props) => {
+  const animate = props.animate ?? true;
+  const { className: customClassName, en, pt } = props;
+  const { language } = useSelector((s: RPropsRedux) => s.Redux);
+
+  const inTransition = language !== "en" && language !== "pt";
+  const currentlyEnglish = language === "en" || language === "en>pt";
+
+  // Get/make default props
+  // const className = customClassName
+  //   ? `${animate ? "" : "noAnimations "}localeText ${customClassName}`
+  //   : `${animate ? "" : "noAnimations "}localeText`;
+  const className = `${
+    styles.localized
+  } ${
+    animate ? styles.animated : ""
+  } ${
+    inTransition ? styles.transition : ""
+  } ${
+    customClassName ?? ""
+  }`;
+
+  return (
+    <span className={className}>
+      {currentlyEnglish ? en : pt}
+    </span>
+  );
+};
+
+export default LocaleText;
