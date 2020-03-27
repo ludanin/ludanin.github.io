ce3944719f741c9a58de92b953ac9a1fcd23e21c
Lukas Danin <lukas.danin@gmail.com>
2020-03-27 11:50:06 -0300

    Implements a class for ModelParagraph

--- src/models/paragraph.ts
+++ src/models/paragraph.ts
@@ -13,8 +13,20 @@
  * ["I have a ", <br/>, "linebreak"]
  * ```
  */
- export interface ModelParagraph {
+export interface ModelParagraph {
   en: string;
   pt: string;
   type?: "normal" | "quotes" | "notes" | "bullet" | "title";
+}
+
+export default class Paragraph implements ModelParagraph {
+  readonly en: ModelParagraph["en"];
+  readonly pt: ModelParagraph["pt"];
+  readonly type: ModelParagraph["type"];
+
+  constructor(args: ModelParagraph) {
+    this.en = args.en;
+    this.pt = args.pt;
+    this.type = args.type ?? "normal";
+  }
 }
