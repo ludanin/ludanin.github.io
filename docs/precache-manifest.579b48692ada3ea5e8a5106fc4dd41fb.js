9bce2d6cad6fdbf0df644b7e88ce6726a5c19d64
Lukas Danin <lukas.danin@gmail.com>
2020-04-05 02:26:04 -0300

    Builds an alpha version for production

---
+++ docs/precache-manifest.579b48692ada3ea5e8a5106fc4dd41fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7a78b3ab4101ea51250ecbb921e7ba0",
+    "url": "https://ludanin.github.io/docs/index.html"
+  },
+  {
+    "revision": "c8e46f34260957b3f7e8",
+    "url": "https://ludanin.github.io/docs/static/css/main.9a1c3938.chunk.css"
+  },
+  {
+    "revision": "54c03d45b3a5aa18378c",
+    "url": "https://ludanin.github.io/docs/static/js/2.359fab0d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "https://ludanin.github.io/docs/static/js/2.359fab0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8e46f34260957b3f7e8",
+    "url": "https://ludanin.github.io/docs/static/js/main.6555a9e3.chunk.js"
+  },
+  {
+    "revision": "83119a14822931cf35de",
+    "url": "https://ludanin.github.io/docs/static/js/runtime-main.3b6c3ced.js"
+  }
+]);
